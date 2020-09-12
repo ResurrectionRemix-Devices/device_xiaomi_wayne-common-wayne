f979a22ee2b4b25c8fcf5928175e4ccbd954ee93
VerNANDo57 <49901327+verNANDo57@users.noreply.github.com>
2020-09-12 21:14:38 +0700

    wayne_common.mk: Remove RRO duplicate

--- wayne_common.mk
+++ wayne_common.mk
@@ -428,10 +428,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(WAYNE_COMMON_PATH)/overlay-device-specific-lineage/lineage-sdk
 
-# RRO configuration
-TARGET_USES_RRO := true
-PRODUCT_ENFORCE_RRO_TARGETS := *
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
